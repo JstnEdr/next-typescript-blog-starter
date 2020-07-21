f8d81f3d6054707a2a747c4488b88fb6d18560bf
DESKTOP-Justin <justineder@gmail.com>
2020-07-20 21:12:34 -0500

    working build

--- src/next.config.js
+++ src/next.config.js
@@ -24,29 +24,39 @@
 // });
 
 /* -------------------------------------------------------------------*/
+//mdxjs.com/getting-started/next
+// https: const withMDX = require('@next/mdx')({
+//   extension: /\.mdx?$/,
+// });
+// module.exports = withMDX({
+//   pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'md', 'mdx'],
+// });
+
+/* -------------------------------------------------------------------*/
+// https://github.com/lorenseanstewart/nextjs-mdx-blog-kit/blob/master/next.config.js
 module.exports = (phase, { defaultConfig }) => {
-  const fs = require("fs");
-  const path = require("path");
-  const { promisify } = require("util");
+  const fs = require('fs');
+  const path = require('path');
+  const { promisify } = require('util');
   const copyFile = promisify(fs.copyFile);
-  const withFonts = require("next-fonts");
-  const images = require("remark-images");
-  const emoji = require("remark-emoji");
-  const withOptimizedImages = require("next-optimized-images");
-  const withMDX = require("@zeit/next-mdx")({
+  const withFonts = require('next-fonts');
+  const images = require('remark-images');
+  const emoji = require('remark-emoji');
+  const withOptimizedImages = require('next-optimized-images');
+  const withMDX = require('@zeit/next-mdx')({
     // parse mdx files
     extension: /\.mdx?$/,
     options: {
       remarkPlugins: [images, emoji],
     },
     // allows for firebase function deployment
-    distDir: "../../dist/functions/next",
+    distDir: '../dist/functions/next',
   });
 
   return withFonts(
     withOptimizedImages(
       withMDX({
-        pageExtensions: ["js", "jsx", "md", "mdx"],
+        pageExtensions: ['js', 'jsx', 'md', 'mdx'],
         exportPathMap: async function (
           defaultPathMap,
           { dev, dir, outDir, distDir, buildId }
@@ -55,19 +65,19 @@ module.exports = (phase, { defaultConfig }) => {
             return defaultPathMap;
           }
           await copyFile(
-            path.join(dir, "my-worker.js"),
-            path.join(outDir, "my-worker.js")
+            path.join(dir, 'my-worker.js'),
+            path.join(outDir, 'my-worker.js')
           );
           return defaultPathMap;
         },
         webpack: function (config, { dev }) {
           config.node = {
-            fs: "empty",
+            fs: 'empty',
           };
           // parse yaml so we can use config.yml
           config.module.rules.push({
             test: /\.ya?ml$/,
-            use: "js-yaml-loader",
+            use: 'js-yaml-loader',
           });
           return config;
         },
