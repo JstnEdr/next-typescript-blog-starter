ea459f13fe591c50cd6d4c2f9ddf18582ad0e034
DESKTOP-Justin <justineder@gmail.com>
2020-07-26 15:07:18 -0500

    updated date on copyright

---
+++ src/components/Footer.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { config } from '../config/config.yml';
+
+export default function Footer() {
+  return (
+    <div className="footer">
+      &copy; {new Date().getFullYear()} {config.author}
+    </div>
+  );
+}
