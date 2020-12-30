8e8f6309e837f6e189dc36859130fafd3c340d03
root <root@DESKTOP-NEKOQ93.localdomain>
2020-12-29 20:32:10 -0600

    deleted old project

--- src-bak/components/AppShell.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import Header from './Header';
-
-export const AppShell = ({ children }) => (
-  <main>
-    <Header />
-    {children}
-  </main>
-);
--- src-bak/pages/about.js
+++
@@ -1,3 +0,0 @@
-import * as React from "react";
-
-export default () => <p>Yeah, that's about it for now</p>;
