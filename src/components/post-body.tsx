cfea997bb83f48899b1dd5d44622a6d17d9f344a
Justin Eder <justineder@gmail.com>
2020-12-31 15:22:23 -0600

    Added Dark example

--- src/components/post-body.tsx
+++ src/components/post-body.tsx
@@ -8,6 +8,10 @@ type Props = {
 const PostBody = ({ content }: Props) => {
   return (
     <div className="max-w-2xl mx-auto">
+      <div className="bg-white dark:bg-gray-800">
+        <h1 className="text-gray-900 dark:text-white">Dark mode is here!</h1>
+        <p className="text-gray-600 dark:text-gray-300">Lorem ipsum...</p>
+      </div>
       <div className={markdownStyles['markdown']}>{content}</div>
     </div>
   );
